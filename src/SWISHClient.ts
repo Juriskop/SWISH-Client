5f42f5a3a8d13636385e794b77bfc0eb7a3a5e20
Mathias Truschel <mtruschel@gmail.com>
2022-10-07 16:38:44 +0200

    implements getProgramCodeAsJson and getProgramCodeAsRawText

--- src/SWISHClient.ts
+++ src/SWISHClient.ts
@@ -1,19 +1,29 @@
 import axios from "axios";
 import {httpDigestAuth} from '@juriskop/axios-http-digest-auth';
 import {SWISHProgramDataJson} from "./types";
+import { joinToUrl } from "./URLProcessor";
 
 export class SWISHClient {
     private axiosInstance = axios.create({});
+   // public baseUrl: string;
 
-    constructor(username: string, password: string, baseUrl: string) {
+    constructor(username: string, password: string, public baseUrl: string) {
+       // baseURL feature does not work with http Digest auth
         httpDigestAuth(this.axiosInstance, {username, password});
+    
+      //  this.baseUrl = baseUrl;
     }
 
     async getProgramCodeAsJson(programName: string): Promise<SWISHProgramDataJson> {
-        throw new Error('Not yet implemented');
+       let response = await this.axiosInstance.get(`${joinToUrl([this.baseUrl, "/p/", programName])}?format=json`);
+    
+        return response.data;
+       
     }
 
     async getProgramCodeAsRawText(programName: string): Promise<string> {
-        throw new Error('Not yet implemented');
+        let response = await this.axiosInstance.get(`${joinToUrl([this.baseUrl, "/p/", programName])}?format=raw`, {responseType:"text"});
+
+        return response.data;
     }
 }
