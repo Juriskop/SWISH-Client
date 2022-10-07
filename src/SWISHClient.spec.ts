ba56fc515ab3d87ecd85b0ee2c3c317352c371bf
Mathias Truschel <mtruschel@gmail.com>
2022-10-07 16:36:34 +0200

    Fix the unit tests

--- src/SWISHClient.spec.ts
+++ src/SWISHClient.spec.ts
@@ -18,9 +18,9 @@ describe('SWISHClient', () => {
     describe('get program code', () => {
         it('can retrieve program code as json', async () => {
             const result = await swish.getProgramCodeAsJson('c6f177aadb4800a7fa146ab948179a97152f1ccd');
-            expect(result.data).toBe('safe_clause(Head, Body) :- \\\\+ predicate_property(Head, built_in), functor(P, Name, _), Name \\\\== call, clause(Head, Body).');
+            expect(result.data).toBe('safe_clause(Head, Body) :- \\+ predicate_property(Head, built_in), functor(P, Name, _), Name \\== call, clause(Head, Body).');
             expect(result.meta).toEqual({
-                "meta": {
+               
                     "author": "Juriskop Projektmitglied",
                     "commit": "c6f177aadb4800a7fa146ab948179a97152f1ccd",
                     "data": "6b8c23b53c3df52fba58af5b974dddc2cfd376d0",
@@ -35,13 +35,13 @@ describe('SWISHClient', () => {
                     "public": true,
                     "time": 1657111287.2864354,
                     "title": "An attempt to define a clause which works with built in predicates without throwing."
-                }
-            });
+                
+            }); 
         });
 
         it('can retrieve program code as text', async () => {
             const result = await swish.getProgramCodeAsRawText('c6f177aadb4800a7fa146ab948179a97152f1ccd');
-            expect(result).toBe('safe_clause(Head, Body) :- \\\\+ predicate_property(Head, built_in), functor(P, Name, _), Name \\\\== call, clause(Head, Body).');
+            expect(result).toBe('safe_clause(Head, Body) :- \\+ predicate_property(Head, built_in), functor(P, Name, _), Name \\== call, clause(Head, Body).');
         })
     });
 });
